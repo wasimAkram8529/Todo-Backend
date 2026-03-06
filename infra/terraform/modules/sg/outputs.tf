67f46ba5c3ee22c6909474027fa28fbeb12a9b31
wasimAkram8529 <codelikearockstar@gamil.com>
2026-03-06 17:04:35 +0000

    Step added in playbook to remove older images, alb module added in terraform, variables renamed in instance, sg modules, data resources added for vpc, subnet

--- infra/terraform/modules/sg/outputs.tf
+++ infra/terraform/modules/sg/outputs.tf
@@ -1,3 +1,9 @@
+
+
 output "tf-backend-sg-id" {
-  value = aws_security_group.tf-backend-sg.id
+  value = aws_security_group.tf-backend-sg["tf-frontend-sg"].id
+}
+
+output "tf-alb-sg-id" {
+  value = aws_security_group.tf-backend-sg["tf-alb-sg"].id
 }
